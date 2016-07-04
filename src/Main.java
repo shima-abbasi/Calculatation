57434b9ee8a7cb26a39e1bfc6977e03a1cfaa9c6
Shima Abbasi <shima.abbasi1992@gmail.com>
2016-07-04 16:34:50 +0430

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.RandomAccessFile;
+import java.io.File;
 
 public class Main {
     Parser parserObj = new Parser();
@@ -9,7 +10,14 @@ public class Main {
 
     }
 
-    public void print() throws FileNotFoundException {
-       RandomAccessFile out = new RandomAccessFile("Out.txt", "rw");
+    public void print() {
+        RandomAccessFile out = null;
+        try {
+            File file = new File("out.txt");
+            out = new RandomAccessFile(file, "rw");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        }
     }
 }
