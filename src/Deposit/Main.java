d868adbb08210cb097590ef2b1129eba286decca
Shima Abbasi <shima.abbasi1992@gmail.com>
2016-07-16 14:44:55 +0430

    Initial commit

--- src/Deposit/Main.java
+++ src/Deposit/Main.java
@@ -8,14 +8,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Parse parseObj = new Parse();
         ArrayList<Deposit> depositArray = parseObj.parseFunction();
-        Collections.sort(depositArray , Collections.reverseOrder());
+        Collections.sort(depositArray, Collections.reverseOrder());
         print(depositArray, "out.txt");
 
     }
+
     public static void print(ArrayList<Deposit> depositArray, String filePath) throws IOException {
-        PrintStream printStream = new PrintStream(new  FileOutputStream("out.txt"));
+        PrintStream printStream = new PrintStream(new FileOutputStream("out.txt"));
         for (Deposit deposit : depositArray) {
-            printStream.println(deposit.getCustomerNumber() + "#" + deposit.getPaidInterest() );
+            printStream.println(deposit.getCustomerNumber() + "#" + deposit.getPaidInterest());
         }
         printStream.close();
     }
