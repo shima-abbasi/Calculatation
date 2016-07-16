917b2da5cd8185b030544ba09bed94c54a68405d
Shima Abbasi <shima.abbasi1992@gmail.com>
2016-07-16 11:44:33 +0430

    Initial commit

---
+++ src/Exceptions/IncorrectValueExceptions.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+/**
+ * Created by Shima Abbasi on 7/16/2016.
+ */
+public class IncorrectValueExceptions extends Exception {
+    public IncorrectValueExceptions(String message) {
+        super(message);
+    }
+}
