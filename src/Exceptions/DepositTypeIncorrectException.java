43740e2c98f590a16aaeabb762ac085b0b55c664
Shima Abbasi <shima.abbasi1992@gmail.com>
2016-07-16 11:34:42 +0430

    Initial commit

--- src/Exceptions/DepositTypeIncorrectException.java
+++ src/Exceptions/DepositTypeIncorrectException.java
@@ -3,5 +3,5 @@ package Exceptions;
 /**
  * Created by Shima Abbasi on 7/16/2016.
  */
-public class DepositTypeIncorrectException {
+public class DepositTypeIncorrectException extends Exception{
 }
--- src/Exceptions/DurationInDaysIncorrectException.java
+++ src/Exceptions/DurationInDaysIncorrectException.java
@@ -3,5 +3,5 @@ package Exceptions;
 /**
  * Created by Shima Abbasi on 7/16/2016.
  */
-public class DurationInDaysIncorrectException {
+public class DurationInDaysIncorrectException extends Exception {
 }
