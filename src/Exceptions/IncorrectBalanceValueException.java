1a5446270b7b3c0a0a1a6d8621a01e2a16acaf14
Shima Abbasi <shima.abbasi1992@gmail.com>
2016-07-16 14:57:21 +0430

    Initial commit

---
+++ src/Exceptions/IncorrectBalanceValueException.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+/**
+ * Created by Shima Abbasi on 7/16/2016.
+ */
+public class IncorrectBalanceValueException extends Exception {
+    public IncorrectBalanceValueException(String message) {
+        super(message);
+    }
+}
---
+++ src/Exceptions/IncorrectDaysValueException.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+/**
+ * Created by Shima Abbasi on 7/16/2016.
+ */
+public class IncorrectDaysValueException extends Exception {
+    public IncorrectDaysValueException(String message) {
+        super(message);
+    }
+}
