47e53e773aeb0410d8f0185aca39d780051192d2
Shima Abbasi <shima.abbasi1992@gmail.com>
2016-07-16 11:20:15 +0430

    Initial commit

--- src/DepositType.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Shima Abbasi on 6/26/2016.
- */
-public abstract class DepositType {
-
-    protected int interestRate = getInterestRate();
-
-    public abstract int getInterestRate();
-}
